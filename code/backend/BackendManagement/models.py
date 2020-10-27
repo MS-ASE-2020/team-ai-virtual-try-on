155e7c94585503f5e33f6eee766ad6f853201001
ForeverFancy <1026816510@qq.com>
2020-10-27 09:49:54 +0800

    [backend] add model

--- code/backend/BackendManagement/models.py
+++ code/backend/BackendManagement/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    return '/pics/user_{}/{}'.format(instance.username, filename)
+
+
+class Customer(models.Model):
+    username = models.CharField('username', max_length=20, primary_key=True)
+    password = models.CharField('password', max_length=20, blank=False)
+    phone_number = models.DecimalField('phone_number', max_digits=11, blank=False)
+    self_pics = models.ImageField('self_pics', upload_to=user_directory_path)
+
+    class Meta:
+        db_table = 'User'
+
+
+class Saler(models.Model):
+    username = models.CharField('username', max_length=20, primary_key=True)
+    password = models.CharField('password', max_length=20, blank=False)
+    phone_number = models.DecimalField('phone_number', max_digits=11, blank=False)
+
+    class Meta:
+        db_table = 'Saler'
+
+
+class Product(models.Model):
+    ID = models.CharField('ID', auto_created=True, primary_key=True)
+    name = models.CharField('name', max_length=20, blank=False)
+    price = models.FloatField('price', default=0.0, blank=False)
+    link = models.URLField('link', max_length=255, default="https://www.taobao.com")
+    overall_score = models.FloatField('overall_score', default=0)
+    number_people_scoring = models.DecimalField('number_people_scoring', default=0)
+    owned_saler = models.ForeignKey(Saler, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Product'
--- code/backend/backend/settings.py
+++ code/backend/backend/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BackendManagement.apps.BackendmanagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
